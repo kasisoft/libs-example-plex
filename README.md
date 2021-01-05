27e5a7262afb0fb5dd961f0d955884a38f524b58
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2021-01-05 17:44:03 +0100

    ~ updated the example code

---
+++ README.md
@@ -0,0 +1,50 @@
+![Plex](https://github.com/kasisoft/plex/workflows/Plex/badge.svg)
+
+# Purpose
+
+Example project for [Plex](https://kasisoft.com/project/plex/)
+
+
+# Infos
+
+* [daniel.kasmeroglu@kasisoft.com](mailto:daniel.kasmeroglu@kasisoft.com)
+* [Github](https://github.com/kasisoft/plex/issues)
+* [GIT](https://github.com/kasisoft/plex.git)
+
+
+# How to try it out?
+
+````
+RunImport src/main/resources/sample001
+or
+RunImport src/main/resources/sample002
+or
+any definition created by you
+````
+
+Expectation: Printout of the extracted data table.
+
+
+# License
+
+MIT License
+
+Copyright (c) 2021 Daniel Kasmeroglu (Kasisoft)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- src/main/java/example/RunImport.java
+++ src/main/java/example/RunImport.java
@@ -1,11 +1,3 @@
-/**
- * Name........: RunImport
- * Description.: Sample application used for the import.
- * Author......: Daniel Kasmeroglu
- * E-Mail......: daniel.kasmeroglu@kasisoft.net
- * Company.....: Kasisoft
- * License.....: LGPL
- */
 package example;
 
 import com.kasisoft.libs.plex.*;
@@ -19,32 +11,33 @@ import java.io.*;
  */
 public class RunImport {
 
-  public static final void main( String[] args ) {
+  public static void main(String[] args) {
 
-    if( args.length == 0 ) {
-      System.err.println( "Missing argument !" );
+    if (args.length == 0) {
+      System.err.println("Missing argument !");
       return;
     }
     
     String   name        = args[0];
-    File     declaration = new File( String.format( "%s.plex", name ) );
+    File     declaration = new File( String.format("%s.plex", name));
     Importer importer    = null;
+    
     try {
-      importer = new Importer( declaration.toURI().toURL() );
-    } catch( MalformedURLException ex ) {
+      importer = new Importer(declaration.toURI().toURL());
+    } catch (MalformedURLException ex) {
       // we've made sure that the resource and thus the URL is correct, so ignore this
       return;
-    } catch( PLEXException ex ) {
+    } catch (Exception ex) {
       // the declaration file was obviously invalid
       ex.printStackTrace();
       return;
     }
     
     try {
-      File       excel    = new File( String.format( "%s.xls", name ) );
-      PlainExcel plex     = importer.runImport( excel );
-      System.out.println( plex );
-    } catch( PLEXException ex ) {
+      File       excel    = new File(String.format("%s.xls", name));
+      PlainExcel plex     = importer.runImport(excel);
+      System.out.println(plex);
+    } catch (Exception ex) {
       // the import failed for some reason
       ex.printStackTrace();
     }
